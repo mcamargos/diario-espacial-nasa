34c9cead1bc3f096d8b921b8898a28d1afa89c4e
Matheus Camargos <camargosdantas@gmail.com>
2025-05-24 13:39:19 -0300

    Atualizando o projeto para o Render.com

--- app.py
+++ app.py
@@ -67,4 +67,6 @@ def get_mars_rover_data():
 if __name__ == '__main__':
     # Define a porta como 5000 por padrão. Debug=True é bom para desenvolvimento,
     # mas deve ser False em produção.
-    app.run(debug=True, port=5000)
+    if __name__ == '__main__':
+     port = int(os.environ.get("PORT", 5000)) # Pega a porta da variável de ambiente 'PORT', ou usa 5000 como fallback
+     app.run(debug=True, host='0.0.0.0', port=port) # host='0.0.0.0' permite que o servidor seja acessível externamente
